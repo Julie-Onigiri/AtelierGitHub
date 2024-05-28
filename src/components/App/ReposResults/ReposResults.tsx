cbb23fe44de696f834a218edd62ce95ac114f73a
Julie-Onigiri <loptibus@gmail.com>
2024-05-28 16:18:21 +0200

    feat forms doesn't work

--- src/components/App/ReposResults/ReposResults.tsx
+++ src/components/App/ReposResults/ReposResults.tsx
@@ -6,29 +6,20 @@ import repos from '../../../data/repos';
 //   repos: Repos[];
 // };
 function ReposResults() {
-  const reposDisplay = repos.items[0];
-
-  return (
-    <section className="result">
-      <article className="result-card ui card">
-        <figure className="result-card__figure image">
-          <img
-            className="result-card__img"
-            src={reposDisplay.owner.avatar_url}
-            alt=""
-          />
-        </figure>
-        <div className="result-card__content content">
-          <p className="result-card__title header">
-            {reposDisplay.owner.login}
-          </p>
-          <p className="result-card__subtitle meta">{reposDisplay.name}</p>
-          <p className="result-card__description description">
-            {reposDisplay.description}
-          </p>
-        </div>
-      </article>
-    </section>
-  );
+  const reposDisplay = repos.items.map((repo) => (
+    <article className="result-card ui card" key={repo.id}>
+      <figure className="result-card__figure image">
+        <img className="result-card__img" src={repo.owner.avatar_url} alt="" />
+      </figure>
+      <div className="result-card__content content">
+        <p className="result-card__title header">{repo.owner.login}</p>
+        <p className="result-card__subtitle meta">{repo.name}</p>
+        <p className="result-card__description description">
+          {repo.description}
+        </p>
+      </div>
+    </article>
+  ));
+  return <section className="result">{reposDisplay}</section>;
 }
 export default ReposResults;
--- src/components/App/SearchBar/SearchBar.tsx
+++ src/components/App/SearchBar/SearchBar.tsx
@@ -1,12 +1,28 @@
 import './index.scss';
+import React, { useState } from 'react';
 
-function SearchBar() {
+type SearchBarProps = {
+  onSearch: string;
+};
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+   onSearch(searchTerm);
+  };
+
   return (
     <section className="search ui raised segment">
-      <form className="search-form">
+      <form onSubmit={handleFormSubmit} className="search-form">
         <div className="search-form__box ui left icon input">
           <i aria-hidden="true" className="search-form__icon search icon" />
           <input
+            value={searchTerm}
+            onChange={handleInputChange}
             className="search-form__input"
             type="text"
             placeholder=" search "
