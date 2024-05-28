e85786b8b0ee42c35284a9142f77599fc8b7ee40
Julie-Onigiri <loptibus@gmail.com>
2024-05-28 10:14:20 +0200

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import logo from '../../assets/logo.svg';
 import 'semantic-ui-css/semantic.min.css';
 import './App.scss';
-import Message from '../Message/Message';
-import ReposResults from '../ReposResults/ReposResults';
-import SearchBar from '../SearchBar/SearchBar';
+import Message from './Message/Message';
+import ReposResults from './ReposResults/ReposResults';
+import SearchBar from './SearchBar/SearchBar';
+import repos from '../../data/repos';
 
 function App() {
   return {};
